464830a6a1e734580fa706fa53cec5fbe9270711
zeel-03 <5877zeelsabhaya@gmail.com>
2026-02-20 11:31:31 +0530

    Add_Loader

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import LandingPage from './Pages/LandingPage'
-
+import Loader from './Component/Loader/Loader'
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {  
+    // Loader 2 second baad hide hoga
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  
+    return () => {
+      clearTimeout(timer);
+    };
+
+  }, []);
   return (
     <>
-      <LandingPage />
+      {loading ? <Loader /> : <LandingPage />}
     </>
   )
 }
